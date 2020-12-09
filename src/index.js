91a49aed51a8c6f4c287b51266cf98b4f0a0c806
lauren <lauren.kroner@gmail.com>
2020-12-09 12:10:55 -0800

    Add console for testing.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 export default function isTouchDevice() {
-  return (
-    !!(typeof window !== 'undefined' &&
+  console.log('HI FROM PACKAGE')
+  const returnable = !(typeof window !== 'undefined' &&
       ('ontouchstart' in window ||
         (window.DocumentTouch &&
           typeof document !== 'undefined' &&
           document instanceof window.DocumentTouch))) ||
     !!(typeof navigator !== 'undefined' &&
-      (navigator.maxTouchPoints || navigator.msMaxTouchPoints))
-  );
+      (navigator.maxTouchPoints || navigator.msMaxTouchPoints));
+  console.log({returnable})
+  return returnable;
 }
